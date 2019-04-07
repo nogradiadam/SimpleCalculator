8e0c5fb5c9030a474c4f0a1ddb5a595014e975cf
Adam Nogradi <nogradiadam@gmail.com>
2019-04-07 14:26:08 +0100

    Created basic calculator functionality

---
+++ Calculator/Model/CalculatorLogic.swift
@@ -0,0 +1,56 @@
+//
+//  CalculatorLogic.swift
+//  Calculator
+//
+//  Created by Adam Nogradi on 06/04/2019.
+//  Copyright © 2019 Adam Nogradi. All rights reserved.
+//
+
+import Foundation
+
+struct CalculatorLogic {
+    
+    private var number: Double?
+    private var intermediateCalculation: (n1: Double, calcMethod: String)?
+    
+    mutating func setNumber(_ number: Double) {
+        self.number = number
+    }
+    
+    mutating func calculate (symbol: String) -> Double? {
+        if let n = number {
+            switch symbol {
+            case "+/-":
+                return n * -1
+            case "%":
+                return n * 0.01
+            case "AC":
+                return 0
+            case "=":
+                return performTwoNumberCalculation(n2: n)
+            default:
+                intermediateCalculation = (n1: n, calcMethod: symbol)
+            }
+        }
+        return nil
+    }
+    
+    private func performTwoNumberCalculation(n2: Double) -> Double? {
+        if let n1 = intermediateCalculation?.n1,
+            let operation = intermediateCalculation?.calcMethod {
+            switch operation {
+            case "+":
+                return n1 + n2
+            case "-":
+                return n1 - n2
+            case "×":
+                return n1 * n2
+            case "÷":
+                return n1 / n2
+            default:
+                fatalError("The operation passed does not match any cases!")
+            }
+        }
+        return nil
+    }
+}
